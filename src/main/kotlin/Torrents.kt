591641dced398e83364bd3934b3947d57c975980
dpozinenko <dpozinenko@playtika.com>
2022-05-07 23:29:27 +0300

    better output

--- src/main/kotlin/Torrents.kt
+++ src/main/kotlin/Torrents.kt
@@ -1,12 +1,12 @@
 private const val separator =
-    "---------------------------------------------------------------------------------------------\n"
+    "--------------------------------------------------------------------\n"
 
 class Torrents(val torrents: List<Torrent>) {
 
     override fun toString() =
         if (torrents.isEmpty()) "No Torrents"
         else torrents.withIndex()
-            .joinToString(separator, separator) { "[${it.index}] ${it.value.name} " }
+            .joinToString(separator, separator) { "[${it.index}] ${it.value.name} \n" }
 
     companion object {
         fun empty() = Torrents(listOf())
