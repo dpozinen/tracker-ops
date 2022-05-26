8c3bf0170b42bdfe784af52ed6f69a53227ebf95
Dariy Pozinenko <dpozinen@gmail.com>
2022-05-26 17:14:14 +0300

    add colors for stats

--- src/main/kotlin/dpozinen/deluge/DelugeTorrents.kt
+++ src/main/kotlin/dpozinen/deluge/DelugeTorrents.kt
@@ -3,10 +3,9 @@ package dpozinen.deluge
 data class DelugeTorrents(val torrents: List<DelugeTorrent>, val stats: Stats) : Iterable<DelugeTorrent> {
 
     data class Stats(val selected: Int, val total: Int,
-                     val paused: Int, val downloading: Int, val seeding: Int,
+                     val downloading: Int, val paused: Int, val seeding: Int,
                      val activeDown: Int, val activeUp: Int,
-                     val ratio: Double,
-                     val downloaded: String, val uploaded: String,
+                     val downloaded: String, val ratio: Double, val uploaded: String,
                      val downSpeed: String, val upSpeed: String
     )
 
