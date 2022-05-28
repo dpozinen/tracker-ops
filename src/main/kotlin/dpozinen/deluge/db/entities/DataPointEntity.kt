10d3a5b567999019c8244457c9d32c363a85e1b5
Dariy Pozinenko <dpozinen@gmail.com>
2022-05-28 23:33:12 +0300

    final db config & ddl

--- src/main/kotlin/dpozinen/deluge/db/entities/DataPointEntity.kt
+++ src/main/kotlin/dpozinen/deluge/db/entities/DataPointEntity.kt
@@ -1,24 +1,28 @@
 package dpozinen.deluge.db.entities
 
-import org.hibernate.annotations.CreationTimestamp
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 import javax.persistence.*
-import javax.persistence.EnumType.STRING
 
+// 70 + 8 + 8 + 8 = 94 bytes
 @Entity
 @EntityListeners(AuditingEntityListener::class)
 class DataPointEntity(
-    @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null,
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long? = null,
 
-    @Column(nullable = false) val torrentId: String,
+    @Column(nullable = false, length = 70)
+    val torrentId: String,
 
-    @Column(nullable = false) @Enumerated(STRING) val graph: Graph,
+    @Column(nullable = false)
+    val graph: Graph,
 
-    @CreatedDate var time: LocalDateTime? = null,
+    @CreatedDate @Column(columnDefinition = "SMALLDATETIME")
+    var time: LocalDateTime? = null,
 
-    @Column(nullable = false) val data: Long
+    @Column(nullable = false)
+    val data: Long
 ) {
 
     enum class Graph {
