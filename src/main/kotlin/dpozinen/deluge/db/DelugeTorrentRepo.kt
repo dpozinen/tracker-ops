1b1f00fc991b7f416edfa15a2a4f3368dbfc5318
Dariy Pozinenko <dpozinen@gmail.com>
2022-06-06 14:46:57 +0300

    finalize stats endpoint

--- src/main/kotlin/dpozinen/deluge/db/DelugeTorrentRepo.kt
+++ src/main/kotlin/dpozinen/deluge/db/DelugeTorrentRepo.kt
@@ -3,8 +3,13 @@ package dpozinen.deluge.db
 import dpozinen.deluge.db.entities.DelugeTorrentEntity
 import org.springframework.context.annotation.Profile
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 @Profile("stats")
-interface DelugeTorrentRepo : JpaRepository<DelugeTorrentEntity, String>
+interface DelugeTorrentRepo : JpaRepository<DelugeTorrentEntity, String> {
+
+    @Query("select id from DelugeTorrentEntity")
+    fun findAllIds(): List<String>
+}
--- src/main/kotlin/dpozinen/deluge/rest/DelugeStatsController.kt
+++ src/main/kotlin/dpozinen/deluge/rest/DelugeStatsController.kt
@@ -3,36 +3,41 @@ package dpozinen.deluge.rest
 
 import dpozinen.deluge.core.DelugeService
 import dpozinen.deluge.core.DelugeStatsService
-import dpozinen.deluge.db.entities.DataPointEntity
-import dpozinen.deluge.domain.DataPoint
 import dpozinen.deluge.domain.Stats
+import org.springframework.context.annotation.Profile
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
 import java.time.LocalDateTime
+import java.time.LocalDateTime.now
 import java.time.ZoneId
+import java.time.ZoneOffset
 import java.util.*
-import javax.websocket.server.PathParam
-import kotlin.time.DurationUnit
-import kotlin.time.ExperimentalTime
 
-@RestController("/deluge/stats")
+@RestController
+@Profile("stats")
 class DelugeStatsController(
     private val statsService: DelugeStatsService,
     private val torrentService: DelugeService,
     private val converter: DelugeConverter,
 ) {
 
-    @GetMapping
+    @GetMapping("/deluge/stats")
     fun stats(
-        @PathParam("torrentIds") torrentIds: Collection<String>,
-        @PathParam("ago") timeAgo: String?,
-        @PathParam("from") timeFrom: LocalDateTime = LocalDateTime.now().minusHours(6),
-        @PathParam("to") timeTo: LocalDateTime = LocalDateTime.now()
+        @RequestParam("torrentIds", defaultValue = "") torrentIds: Set<String>,
+        @RequestParam("ago", required = false) timeAgo: String?,
+        @RequestParam("from") timeFrom: String?,
+        @RequestParam("to") timeTo: String?
     ): Stats {
         val stats =  Optional.ofNullable(timeAgo)
-            .map { statsService.stats(torrentIds, converter.toLocalDateTime(it), timeTo) }
-            .orElseGet { statsService.stats(torrentIds, timeFrom, timeTo) }
+            .map { statsService.stats(torrentIds, converter.toLocalDateTime(it), now()) }
+            .orElseGet {
+                statsService.stats(
+                    torrentIds,
+                    LocalDateTime.parse(timeFrom) ?: now().minusHours(6),
+                    LocalDateTime.parse(timeTo) ?: now())
+            }
 
         val torrents = torrentService.allTorrents()
 
