29fb39f7c7a0cd9ca04a99500fade3ac9aebf2a7
Dariy Pozinenko <dpozinen@gmail.com>
2022-06-10 11:37:22 +0300

    add rarbg

--- src/test/kotlin/tracker/ParsingTest.kt
+++ src/test/kotlin/tracker/ParsingTest.kt
@@ -3,6 +3,8 @@ package tracker
 import Data.*
 import dpozinen.tracker.TrackerParser
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.data.Index
+import org.assertj.core.data.Index.atIndex
 import kotlin.test.Test
 import java.nio.file.Files
 import java.nio.file.Path
@@ -23,6 +25,22 @@ class ParsingTest {
         val torrent = TrackerParser.OneThreeThree().parseTorrentPage(body)
 
         assertThat(torrent).isEqualTo(OneThreeThree.PAGE_EXPECTED_TORRENT)
+    }
+
+    @Test
+    fun `should parse search page rarbg`() {
+        val body = Files.readString(Path.of(Rarbg.SEARCH_PAGE_PATH))
+        val torrents = TrackerParser.Rarbg().parseSearch(body)
+
+        assertThat(torrents.torrents).contains(Rarbg.SEARCH_EXPECTED_TORRENT, atIndex(0))
+    }
+
+    @Test
+    fun `should parse torrent page rarbg`() {
+        val body = Files.readString(Path.of(Rarbg.TORRENT_PAGE_PATH))
+        val torrent = TrackerParser.Rarbg().parseTorrentPage(body)
+
+        assertThat(torrent).isEqualTo(Rarbg.PAGE_EXPECTED_TORRENT)
     }
 
 }
