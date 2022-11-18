1bf51f014345cab27bdcc22aee9bf6bf584b6337
Dariy Pozinenko <dpozinen@gmail.com>
2022-11-18 13:26:07 +0200

    add logs

--- src/main/kotlin/dpozinen/deluge/core/DelugeStatsService.kt
+++ src/main/kotlin/dpozinen/deluge/core/DelugeStatsService.kt
@@ -3,6 +3,7 @@ package dpozinen.deluge.core
 import dpozinen.deluge.db.MigrationRepository
 import dpozinen.deluge.kafka.StatsKafkaProducer
 import dpozinen.deluge.rest.DelugeConverter
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.annotation.Profile
@@ -20,6 +21,8 @@ class DelugeStatsService(
     private val performMigration: Boolean
 ) {
 
+    private val log = KotlinLogging.logger {}
+
     fun collectStats() {
         val stats = converter.convert(delugeService.allTorrents())
 
@@ -31,9 +34,14 @@ class DelugeStatsService(
         if (performMigration) {
             for (i in 0..50_000) {
                 val stats = migrationRepository.findAll(i * 500)
-                if (stats.isEmpty()) break
-                else producer.send(stats)
+                if (stats.isEmpty()) {
+                    break
+                } else {
+                    log.info { "Sending ${stats.size} to kafka" }
+                    producer.send(stats)
+                }
             }
+            log.info { "Migration complete" }
         }
     }
 
