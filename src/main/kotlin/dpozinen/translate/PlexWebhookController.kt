97d0ae581177dcc521b1409987c6bb334bc3785a
Dariy Pozinenko <dpozinen@gmail.com>
2023-11-05 22:05:06 +0200

    different event name

--- src/main/kotlin/dpozinen/translate/PlexWebhookController.kt
+++ src/main/kotlin/dpozinen/translate/PlexWebhookController.kt
@@ -1,6 +1,9 @@
 package dpozinen.translate
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import mu.KotlinLogging.logger
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
@@ -12,15 +15,16 @@ class PlexWebhookController(private val translationService: TranslationService) 
     private val log = logger {}
     private val objectMapper = jacksonObjectMapper()
 
+    @OptIn(DelicateCoroutinesApi::class)
     @PostMapping("/api/callbacks/plex-webhook")
-    fun consume(@RequestPart body: MultipartFile) {
-        val event = body.bytes.contentToString()
+    fun consume(@RequestPart thumb: MultipartFile) {
+        val event = thumb.bytes.contentToString()
             .let { objectMapper.readValue(it, Map::class.java) }
 
         log.info { "Plex event: ${objectMapper.writeValueAsString(event)}" }
 
         event.takeIf { (it["event"] ?: "") == "library.new" }
-            ?.also { translationService.process(it) }
+            ?.also { GlobalScope.launch {  translationService.process(it) } }
             ?: log.debug { "Ignored event $event" }
     }
 }
