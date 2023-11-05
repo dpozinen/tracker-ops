d4ae145ab16228aafbc6fcff999f9648a8bb8e43
Dariy Pozinenko <dpozinen@gmail.com>
2023-11-05 22:52:41 +0200

    bababa

--- src/main/kotlin/dpozinen/translate/PlexWebhookController.kt
+++ src/main/kotlin/dpozinen/translate/PlexWebhookController.kt
@@ -3,7 +3,9 @@ package dpozinen.translate
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import kotlinx.coroutines.DelicateCoroutinesApi
 import mu.KotlinLogging.logger
+import org.springframework.http.codec.multipart.Part
 import org.springframework.stereotype.Controller
+import org.springframework.util.MultiValueMap
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
@@ -16,8 +18,8 @@ class PlexWebhookController(
 
     @OptIn(DelicateCoroutinesApi::class)
     @PostMapping("/api/callbacks/plex-webhook")
-    fun consume(@RequestBody thumb: Any) {
-        log.info { "Plex event: ${objectMapper.writeValueAsString(thumb)}" }
+    fun consume(@RequestBody parts: MultiValueMap<String, Part>) {
+        log.info { "Plex event: ${objectMapper.writeValueAsString(parts)}" }
 //        objectMapper.readValue(thumb, Map::class.java)
 //        val event = thumb.bytes.contentToString()
 //            .let {  }
@@ -27,4 +29,5 @@ class PlexWebhookController(
 //            ?.also { GlobalScope.launch {  translationService.process(it) } }
 //            ?: log.debug { "Ignored event $event" }
     }
+
 }
