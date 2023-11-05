373d49111297d0c6818aa533de6530ce56eb6eab
Dariy Pozinenko <dpozinen@gmail.com>
2023-11-05 22:34:58 +0200

    aaaa

--- src/main/kotlin/dpozinen/translate/PlexWebhookController.kt
+++ src/main/kotlin/dpozinen/translate/PlexWebhookController.kt
@@ -5,8 +5,7 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 import mu.KotlinLogging.logger
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.bind.annotation.RequestBody
 
 @Controller
 class PlexWebhookController(
@@ -17,11 +16,12 @@ class PlexWebhookController(
 
     @OptIn(DelicateCoroutinesApi::class)
     @PostMapping("/api/callbacks/plex-webhook")
-    fun consume(@RequestPart thumb: MultipartFile) {
-        val event = thumb.bytes.contentToString()
-            .let { objectMapper.readValue(it, Map::class.java) }
+    fun consume(@RequestBody thumb: Any) {
+        log.info { "Plex event: ${objectMapper.writeValueAsString(thumb)}" }
+//        objectMapper.readValue(thumb, Map::class.java)
+//        val event = thumb.bytes.contentToString()
+//            .let {  }
 
-        log.info { "Plex event: ${objectMapper.writeValueAsString(event)}" }
 
 //        event.takeIf { (it["event"] ?: "") == "library.new" }
 //            ?.also { GlobalScope.launch {  translationService.process(it) } }
