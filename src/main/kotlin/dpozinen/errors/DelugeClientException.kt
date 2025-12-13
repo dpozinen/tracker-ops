6aa20b404212211fafa9f213523d180fddd3816a
Dariy Pozinenko <dpozinen@gmail.com>
2025-12-13 16:33:13 +0000

    boot 3.4

--- src/main/kotlin/dpozinen/errors/DelugeClientException.kt
+++ src/main/kotlin/dpozinen/errors/DelugeClientException.kt
@@ -2,12 +2,13 @@ package dpozinen.errors
 
 import feign.Response
 import feign.RetryableException
+import java.util.Date
 
 
 open class DelugeRetryableException(response: Response) :
     RetryableException(response.status(),
         "", response.request().httpMethod(),
-        null, response.request()
+        null as Date?, response.request()
     )
 
 class DelugeSessionExpiredException(response: Response) : DelugeRetryableException(response)
